a71b3e878a85fc9f6066b01ac6f3096510c4c572
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-12-03 10:44:14 -0800

    Completed

--- src/11_args.py
+++ src/11_args.py
@@ -14,8 +14,8 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-def f2(*arg):
-    return sum(arg)
+def f2(*args):
+    return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -35,7 +35,7 @@ print(f2(*a))    # Should print 22
 def f3(x ,y=0):
     if y==0:
         return x+1
-    return x+y
+    return (x+y)
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -55,6 +55,10 @@ print(f3(8))     # Should print 9
 # Should print
 # key: a, value: 12
 # key: b, value: 30
+def f4(**kwargs):
+    for key,value in kwargs.items():
+        print("key:", f'{key},',"value:", value)
+
 f4(a=12, b=30)
 
 # Should print
@@ -69,4 +73,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
